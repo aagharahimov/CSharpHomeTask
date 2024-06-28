ffe5f8e6880be187e82be8d26e5b35008f3e7d30
aagharahimov <aghasalimaa@code.edu.az>
2024-06-29 00:06:00 +0400

    OOP modified

---
+++ #hometask_Collections/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("string daxil edin: ");
+        string str = Console.ReadLine();
+        Console.WriteLine(Stack.Reverse(str));
+    }
+}
+
+class Stack
+{
+    private int top;
+    private int capacity;
+    private char[] array;
+
+    public Stack(int capacity)
+    {
+        this.capacity = capacity;
+        array = new char[capacity];
+        this.top = -1;
+    }
+
+    public bool IsFull()
+    {
+        return this.top == this.capacity - 1;
+    }
+
+    public bool IsEmpty()
+    {
+        return this.top == -1;
+    }
+
+    public void Push(char value)
+    {
+        if (!this.IsFull())
+        {
+            this.array[++this.top] = value;
+        }
+    }
+
+    public char Pop()
+    {
+        return this.IsEmpty() ? '\u0000' : this.array[this.top--];
+    }
+
+    public static string Reverse(string str)
+    {
+        char[] charArr = str.ToCharArray();
+        int size = charArr.Length;
+        Stack stack = new Stack(size);
+
+        for (int i = 0; i < size; i++)
+        {
+            stack.Push(charArr[i]);
+        }
+
+        for (int i = 0; i < size; ++i)
+        {
+            charArr[i] = stack.Pop();
+        }
+
+        return new string(charArr);
+    }
+}
---
+++ delegates/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Delegates
+{
+    public static class Program
+    {
+        
+        delegate bool Check<T>(T item);
+        public static void Main(string[] args)
+        {
+            var array = new int[] { 1, 2, 3, 4, -4, -8, -10 };
+
+            
+            Console.WriteLine(Sum(array, IsNegative));
+
+            Check<string> checkStr = CheckLengthBiggerThan;
+            Console.WriteLine(checkStr.Invoke("salam")); 
+
+        }
+
+        static int Sum(int[] array, Check<int> check)
+        {
+            int sum = 0;
+
+            foreach (var arrayItem in array)
+            {
+                if (check(arrayItem))
+                {
+                    sum += arrayItem;
+                }
+
+            }
+            return sum;
+        }
+
+
+        public static bool IsOdd(int number)
+        {
+            return (number % 2 != 0);
+        }
+
+        public static bool IsEven(int number)
+        {
+            return (number % 2 == 0);
+        }
+
+        public static bool IsNegative(int number)
+        {
+            return (number <0);
+        }
+
+        public static bool CheckLengthBiggerThan(string word)
+        {
+            return (word.Length < 5);
+        }
+    }
+}
